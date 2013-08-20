b56437f6df76235f8217052ddd69925ce19eba6d
jhardy77 <jhardy@hardygroupintl.com>
2013-08-20 15:55:26 +1000

    20/8/13 1600

--- src/com/thenewboston/travis/TextPlay.java
+++ src/com/thenewboston/travis/TextPlay.java
@@ -1,8 +1,10 @@
 package com.thenewboston.travis;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,8 +22,10 @@ public class TextPlay extends Activity {
 		Button chkCmd = (Button) findViewById(R.id.b_Results);
 		final ToggleButton passTog = (ToggleButton) findViewById(R.id.tb_password);
 		final EditText input = (EditText) findViewById(R.id.et_Commands);
-		TextView display = (TextView) findViewById(R.id.tv_Results);
+		final TextView display = (TextView) findViewById(R.id.tv_Results);
 		
+		
+		//Toggle visable password on and off
 		passTog.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -34,7 +38,34 @@ public class TextPlay extends Activity {
 				
 			}
 		});
+		
+		
+		chkCmd.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				String check = input.getText().toString();
+				
+				display.setText(check);
+				
+				if (check.contentEquals("left")) {
+					display.setGravity(Gravity.LEFT);
+				} else if (check.contentEquals("center")) {
+					display.setGravity(Gravity.CENTER);
+				} else if (check.contentEquals("right")) {
+					display.setGravity(Gravity.RIGHT);
+				} else if (check.contentEquals("blue")) {
+					display.setTextColor(Color.BLUE);
+				} else if (check.contentEquals("WTF")) {
+					display.setText("WTF");				
+				} else {
+					display.setText("invalid");
+					display.setGravity(Gravity.CENTER);
+				}
+			}
+		});
 
 	}
-
 }
