798cb2eb0c9b73ff8282bcb776dd37edf73f4323
jhardy77 <jhardy@hardygroupintl.com>
2013-08-23 15:13:14 +1000

    23/8/13 1600

--- src/com/thenewboston/travis/Email.java
+++ src/com/thenewboston/travis/Email.java
@@ -1,31 +1,37 @@
 package com.thenewboston.travis;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class Email extends Activity implements View.OnClickListener {
+public class Email extends Activity implements OnClickListener {
 
 	EditText personsEmail, intro, personsName, stupidThings, hatefulAction, outro;
 	String emailAdd, beginning, name, stupidAction, hatefulAct, out;
 	Button sendEmail;
+
+	
+//////////////////////////////////////////////////////////////////////////////////////
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.email);
-		initializeVars();
-		sendEmail.setOnClickListener(this);
+		initialiseVars();
+		sendEmail.setOnClickListener(this);		
 	}
 
 	
-////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
 	
 	
-	private void initializeVars() {
+	private void initialiseVars() {
 		// TODO Auto-generated method stub
 		personsEmail = (EditText) findViewById(R.id.etEmails);
 		intro = (EditText) findViewById(R.id.etIntro);
@@ -33,17 +39,17 @@ public class Email extends Activity implements View.OnClickListener {
 		stupidThings = (EditText) findViewById(R.id.etThings);
 		hatefulAction = (EditText) findViewById(R.id.etAction);
 		outro = (EditText) findViewById(R.id.etOutro);
-		sendEmail = (Button) findViewById(R.id.bSentEmail);
+		sendEmail = (Button) findViewById(R.id.bSentEmail);		
 	}
 
 	
-////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
 
-
-	public void onClick(View v) {
+	
+	@Override
+	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
-
-		convertEditTextVarsIntoStringsAndYesThisIsAMethodWeCreated();
+		convertTextToStrings();
 		String emailaddress[] = { emailAdd };
 		String message = "Well hello "
 				+ name
@@ -57,13 +63,20 @@ public class Email extends Activity implements View.OnClickListener {
 				+ out
 				+ ".  Oh also if you get bored you should check out www.mybringback.com"
 				+ '\n' + "PS. I think I love you...   :( ";
+		
+		Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+		emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, emailaddress);
+		emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "I hate you!");
+		emailIntent.setType("plain/text");
+		emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, message);
+		startActivity(emailIntent);
 	}
 
 	
-////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
 
 
-	private void convertEditTextVarsIntoStringsAndYesThisIsAMethodWeCreated() {
+	private void convertTextToStrings() {
 		// TODO Auto-generated method stub
 		emailAdd = personsEmail.getText().toString();
 		beginning = intro.getText().toString();
@@ -74,14 +87,18 @@ public class Email extends Activity implements View.OnClickListener {
 	}
 
 	
-////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
 
-
+	
 	@Override
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
 		finish();
 	}
+	
+
+//////////////////////////////////////////////////////////////////////////////////////
+
 
 }
