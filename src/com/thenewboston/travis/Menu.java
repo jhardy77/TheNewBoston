933558d66a7da7d81fff11db3344b13a36d823f9
jhardy77 <jhardy@hardygroupintl.com>
2013-09-09 16:06:03 +1000

    9/9/13 1600

--- src/com/thenewboston/travis/Menu.java
+++ src/com/thenewboston/travis/Menu.java
@@ -4,6 +4,7 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -55,7 +56,7 @@ public class Menu extends ListActivity {
 	
 //////////////////////////////////////////////////////////////////////////////////////
 	
-	
+	//video 51
 	@Override
 	public boolean onCreateOptionsMenu(android.view.Menu menu) {
 		// TODO Auto-generated method stub
@@ -69,5 +70,31 @@ public class Menu extends ListActivity {
 
 
 	
+
 	
+//////////////////////////////////////////////////////////////////////////////////////
+	
+	//Video 52
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		
+		case R.id.aboutus:
+			Intent i = new Intent("com.thenewboston.travis.ABOUT");
+			startActivity(i);
+			
+			break;
+			
+		case R.id.preferences:
+			Intent p = new Intent("com.thenewboston.travis.PREFERENCES");
+			startActivity(p);
+			
+			break;
+		}
+		//Linked to the boolean of this method, needs to return false if aboutus or preferences aren't selected.
+		return false;
+
+	}
+
 }
