7f5b21d1fa10e9afc71d149655b30d79129bc84b
jhardy77 <jhardy@hardygroupintl.com>
2013-09-27 15:30:51 +1000

    29/9/13 1600

--- src/com/thenewboston/travis/HotOrNot.java
+++ src/com/thenewboston/travis/HotOrNot.java
@@ -1,26 +1,47 @@
 package com.thenewboston.travis;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class HotOrNot {
 
-	// Setup of columns
+	// Setup of column details
 	public static final String KEY_ROWID = "_id";
 	public static final String KEY_NAME = "persons_name";
 	public static final String KEY_HOTNESS = "persons_hotness";
 
 	// Setup of database details
-	private static final String DATABASE_NAME = "hotornotdb";
-	private static final String DATABASE_TABLE = "peopletable";
+	private static final String DATABASE_NAME = "HotOrNotdb";
+	private static final String DATABASE_TABLE = "peopleTable";
 	private static final int DATABASE_VERSION = 1;
 
+	
 	private DbHelper ourHelper;
 	private final Context ourContext;
 	private SQLiteDatabase ourDatabase;
 
+	
+	public HotOrNot(Context c) {
+		ourContext = c;
+	}
+
+	
+	public HotOrNot open() throws SQLException {
+		ourHelper = new DbHelper(ourContext);
+		ourDatabase = ourHelper.getWritableDatabase();
+		return this;
+	}
+	
+	//Closes this class
+	public void close() {
+		ourHelper.close();
+	}
+
+	
 	private static class DbHelper extends SQLiteOpenHelper {
 
 		public DbHelper(Context context) {
@@ -31,27 +52,35 @@ public class HotOrNot {
 		@Override
 		public void onCreate(SQLiteDatabase db) {
 			// TODO Auto-generated method stub
-			db.execSQL("CREATE TABLE " + DATABASE_TABLE + " (" +
-					KEY_ROWID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-					KEY_NAME + " TEXT NOT NULL, " +
-					KEY_HOTNESS + " TEXT NOT NULL);"					
-			);
-			
+			db.execSQL("CREATE TABLE " + DATABASE_TABLE + " (" + KEY_ROWID
+					+ " INTEGER PRIMARY KEY AUTOINCREMENT, " + KEY_NAME
+					+ " TEXT NOT NULL, " + KEY_HOTNESS + " TEXT NOT NULL);");
+			// CREATE TABLE HotOrNotdb (
+			// _id INTEGER PRIMARY KEY AUTOINCREMENT,
+			// persons_name TEXT NOT NULL,
+			// persons_hotness TEXT NOT NULL
+			// );
 		}
 
-		
-		
-		
 		@Override
 		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 			// TODO Auto-generated method stub
-
+			db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+			onCreate(db);
 		}
 
 	}
 
-	public HotOrNot(Context c) {
-		ourContext = c;
+
+	public long createEntry(String getName, String getHotness) {
+		// TODO Auto-generated method stub
+		ContentValues cv = new ContentValues();
+		cv.put(KEY_NAME, getName);
+		cv.put(KEY_HOTNESS, getHotness);
+		return ourDatabase.insert(DATABASE_TABLE, null, cv);
 	}
 
+	
+	
+	
 }
--- src/com/thenewboston/travis/SQLiteExample.java
+++ src/com/thenewboston/travis/SQLiteExample.java
@@ -1,11 +1,14 @@
 package com.thenewboston.travis;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class SQLiteExample extends Activity implements OnClickListener {
 
@@ -51,10 +54,35 @@ public class SQLiteExample extends Activity implements OnClickListener {
 		
 		case R.id.b_sqlUpdateData:
 			
+			boolean didItWork = true;
+			try {
+				String getName = name.getText().toString();
+				String getHotness = hotness.getText().toString();
+				
+				HotOrNot entry = new HotOrNot(SQLiteExample.this);
+				entry.open();
+				entry.createEntry(getName, getHotness);
+				
+				entry.close();
+				
+			} catch (Exception e) {
+				didItWork = false;
+			} finally {
+				if (didItWork) {
+					Dialog d = new Dialog(this);
+					d.setTitle("Heck Yea!");
+					TextView  tv = new TextView(this);
+					tv.setText("Success");
+					d.setContentView(tv);
+					d.show();
+				}
+			}
+			
 			break;
 			
 		case R.id.b_sqlView:
-			
+			Intent i = new Intent("com.thenewboston.travis.SQLVIEW");
+			startActivity(i);
 			break;
 			
 		}
